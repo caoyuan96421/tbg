3d8fa7c77cad9aa0b05ecc44d5938c7b5a3fada2
Yuan Cao <caoyuan@mit.edu>
2015-01-15 14:06:44 -0500

    a

--- K.m
+++ K.m
@@ -1,7 +1,7 @@
 function K = K(a, th)
 %K(a,th) calculates the K point of a graphene lattice
 %   Detailed explanation goes here
-sinth = sin(th);
+sinth = sin(th); 
 costh = cos(th);
 K = [costh, -sinth; sinth, costh] * [2*pi/3/a; 2*pi/3/sqrt(3)/a];
 
